30574b421f6aa2ca882efeae33d7265e69c09522
Ana7K <anakitesashvili4@gmail.com>
2025-10-30 11:11:44 +0400

    nth commit

--- lesson69/src/App.tsx
+++ lesson69/src/App.tsx
@@ -1,11 +1,21 @@
 import About from "./components/About.tsx";
+import Contact from "./components/Contact.tsx";
+import Features from "./components/Features.tsx";
+import Footer from "./components/Footer.tsx";
 import Hero from "./components/Hero.tsx";
+import Navbar from "./components/Navbar.tsx";
+import Story from "./components/Story.tsx";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
       <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   );
 };
