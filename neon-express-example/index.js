81ef4eccfeb49b44cb80497ee77f48fd768d5275
Ana7K <anakitesashvili4@gmail.com>
2025-12-18 12:32:58 +0400

    nth commit

--- neon-express-example/index.js
+++ neon-express-example/index.js
@@ -1,17 +1,47 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { neon } from '@neondatabase/serverless';
+import express from "express";
+import dotenv from "dotenv";
+import { neon } from "@neondatabase/serverless";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4242;
 
-app.get('/', async (_, res) => {
-  const sql = neon(`${process.env.DATABASE_URL}`);
+app.use(express.json());
+
+const sql = neon(process.env.DATABASE_URL);
+
+app.get("/", async (_, res) => {
   const response = await sql`SELECT version()`;
   const { version } = response[0];
   res.json({ version });
+});
+
+app.get("/items", async (_, res) => {
+  try {
+    const items = await sql``;
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to get items" });
+  }
+});
+
+app.post("/items", async (req, res) => {
+  try {
+    const { name, age } = req.body;
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to create items" });
+  }
+});
+
+app.put("/items/:id", async (req, res) => {
+  try {
+    const { name, age } = req.body;
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to update items" });
+  }
 });
 
 app.listen(PORT, () => {
